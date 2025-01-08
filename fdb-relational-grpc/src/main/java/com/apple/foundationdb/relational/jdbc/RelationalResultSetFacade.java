1b757a5eef756f180437ca16cbd8562ce0c85973
ohad <ozeliger@apple.com>
2025-01-08 13:45:30 -0500

    Move continuation to separate proto file, store in response

--- fdb-relational-grpc/src/main/java/com/apple/foundationdb/relational/jdbc/RelationalResultSetFacade.java
+++ fdb-relational-grpc/src/main/java/com/apple/foundationdb/relational/jdbc/RelationalResultSetFacade.java
@@ -263,7 +263,7 @@ class RelationalResultSetFacade implements RelationalResultSet {
     @ExcludeFromJacocoGeneratedReport
     @Nonnull
     public Continuation getContinuation() throws SQLException {
-        return new RelationalGrpcContinuation(delegate.getContinuation());
+        return new RelationalRpcContinuation(delegate.getContinuation());
     }
 
     @Override
