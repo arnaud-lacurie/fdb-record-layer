556b385e58334f9681d02904f8fa4cae8837d735
ohad <ozeliger@apple.com>
2025-01-10 12:00:40 -0500

    Fail continuation if the result set is not exhausted

--- fdb-relational-grpc/src/main/java/com/apple/foundationdb/relational/jdbc/RelationalResultSetFacade.java
+++ fdb-relational-grpc/src/main/java/com/apple/foundationdb/relational/jdbc/RelationalResultSetFacade.java
@@ -72,6 +72,10 @@ class RelationalResultSetFacade implements RelationalResultSet {
         return ++rowIndex < rows;
     }
 
+    public boolean hasNext() {
+        return rowIndex < rows;
+    }
+
     @Override
     public boolean isClosed() throws SQLException {
         return this.closed;
@@ -263,7 +267,9 @@ class RelationalResultSetFacade implements RelationalResultSet {
     @ExcludeFromJacocoGeneratedReport
     @Nonnull
     public Continuation getContinuation() throws SQLException {
-        // TODO: Fail if the result set is not exhausted
+        if (hasNext()) {
+            throw new SQLException("Continuation can only be returned for the last row");
+        }
         return new RelationalRpcContinuation(delegate.getContinuation());
     }
 
